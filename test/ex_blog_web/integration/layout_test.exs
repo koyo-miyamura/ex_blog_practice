46c1dcc90e61598612b43bee83f52904f19c247d
koyo <nexus9071@gmail.com>
2018-11-01 04:07:47 +0900

    layoutの統合テスト追加

--- test/ex_blog_web/integration/layout_test.exs
+++ test/ex_blog_web/integration/layout_test.exs
@@ -11,19 +11,34 @@ defmodule ExBlogWeb.LayoutTest do
   end
 
   describe "get /" do
-    test "without login", %{conn: conn} do
+    test "without login", %{conn: conn, user: user} do
       conn = get conn, "/"
+
+      # html_responce is \ escaped like \"hogehoge\", not "hogehoge"
+      # so we need \ before "
       assert html_response(conn, 200) =~ "Simple blog by Phoenix"
-      refute html_response(conn, 200) =~ "<a href=\"#{article_path(conn, :index)}\">"
+      assert html_response(conn, 200) =~ "href=\"#{user_path(conn, :new)}\""
+      assert html_response(conn, 200) =~ "href=\"#{page_path(conn, :index)}\""
+      assert html_response(conn, 200) =~ "href=\"#{session_path(conn, :new)}\""
+
+      refute html_response(conn, 200) =~ "href=\"#{article_path(conn, :index)}\""
+      refute html_response(conn, 200) =~ "href=\"#{user_path(conn, :index)}\""
+      refute html_response(conn, 200) =~ "href=\"#{user_path(conn, :show, user)}\""
+      refute html_response(conn, 200) =~ "href=\"#{session_path(conn, :delete)}\""
     end
     test "logged in", %{conn: conn, user: user} do
       conn =
         conn
         |> login_user(user)
         |> get("/")
-      # html_responce is \ escaped like \"hogehoge\", not "hogehoge"
-      # so we need \ before "
-      assert html_response(conn, 200) =~ "<a href=\"#{article_path(conn, :index)}\">"
+      assert get_flash(conn)
+      refute html_response(conn, 200) =~ "href=\"#{user_path(conn, :new)}\""
+      refute html_response(conn, 200) =~ "href=\"#{session_path(conn, :new)}\""
+
+      assert html_response(conn, 200) =~ "href=\"#{article_path(conn, :index)}\""
+      assert html_response(conn, 200) =~ "href=\"#{user_path(conn, :index)}\""
+      assert html_response(conn, 200) =~ "href=\"#{user_path(conn, :show, user)}\""
+      assert html_response(conn, 200) =~ "href=\"#{session_path(conn, :delete)}\""
     end
   end
 
